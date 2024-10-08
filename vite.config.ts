49b595a5eb8b0c7b41b85b2f8f8857ea9770c590
Patryk Dwórznik <patryk@dworznik.com>
2024-10-08 12:40:38 +0900

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-    plugins: [
-        sveltekit(),
-         nodePolyfills({
-        protocolImports: true,
-    }),],
-    optimizeDeps: {
-        esbuildOptions: {
-          // Node.js global to browser globalThis
-          define: {
-            global: 'globalThis',
-          },
-          // Enable esbuild polyfill plugins
-          plugins: [
-            NodeGlobalsPolyfillPlugin({
-              buffer: true,
-              process: true,
-            }),
-          ],
-        },
-      },
-      resolve: {
-        alias: {
-          // These aliases make sure that the polyfills are used
-          stream: 'stream-browserify',
-          buffer: 'buffer',
-          process: 'process/browser',
-          util: 'util',
-        },
-      },
+    plugins: [sveltekit()]
 });
